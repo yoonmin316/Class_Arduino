182e54acdbb3f6ced8ca7a2d5fc93f83005537af
yoonmin316 <43337144+yoonmin316@users.noreply.github.com>
2024-08-09 13:12:13 +0900

    Add files via upload

---
+++ 과제/code_jiyoon/code_jiyoon.ino
@@ -0,0 +1,17 @@
+#include ? //서보모터 라이브러리 호출
+
+#define pin_sv1  ?  //서보모터1 핀번호
+#define pin_sv2  ?  //서보모터2 핀번호
+
+#define pin_pm1  ?  //가변저항1 핀번호
+#define pin_pm2  ?  //가변저항2 핀번호
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
