b55054b1901fe28df82d8b05db75aec70f3ea4c6
yoonmin316 <43337144+yoonmin316@users.noreply.github.com>
2024-08-11 14:44:30 +0900

    Update code_jiyoon.ino

--- 과제/code_jiyoon/code_jiyoon.ino
+++ 과제/code_jiyoon/code_jiyoon.ino
@@ -15,6 +15,13 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int input1 = ?  //가변저항1 입력 (힌트: a로 시작하는 함수) 
+  int input2 = ?  //가변저항2 입력 (힌트: a로 시작하는 함수) 
+  int angle1 = ?  //입력값을 각도로 맵핑하기!
+  int angle2 = ?  //입력값을 각도로 맵핑하기!
 
+  ?  //해당 각도만큼 서보모터1 움직이기!!
+  ?  //해당 각도만큼 서보모터2 움직이기!!
+
+  delay(200);
 }
