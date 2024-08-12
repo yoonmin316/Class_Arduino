2cb889de8329305cfa02d3bf941e1e35b99a142d
yoonmin316 <43337144+yoonmin316@users.noreply.github.com>
2024-08-12 12:27:57 +0900

    Add files via upload

---
+++ 과제_8_12/code/code.ino
@@ -0,0 +1,19 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int grade = 85;
+  if(grade >= 90 && grade <= 100){  //여기까지 힌트
+
+  }
+  else if(){
+
+  }
+  else if(){
+
+  }
+  else{
+
+  }
+}
