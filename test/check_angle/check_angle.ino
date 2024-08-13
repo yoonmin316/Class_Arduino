71e4abb225e1a6e47abb71de742c7283625336d9
yoonmin316 <43337144+yoonmin316@users.noreply.github.com>
2024-08-14 08:41:01 +0900

    Add files via upload

---
+++ test/check_angle/check_angle.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+//함수 여기에 만들기 (정의)
+//각도를 입력받아
+//예각이면 -> "예각" return
+//직각이면 -> "직각" return
+//둔각이면 -> "둔각" return
+
+void loop() {
+  int angle = 80;  //각도 변수
+  Serial.println(?);  //여기에 함수 호출
+}
